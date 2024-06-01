053d0cc8fbf42f668a17707d3c1188b34602165e
Umme-Nahida <mysuccessmission@gmail.com>
2024-06-01 18:14:49 +0600

    added details layout page

--- src/app/[news]/[newsId]/page.js
+++ src/app/[news]/[newsId]/page.js
@@ -1,28 +1,49 @@
 import { getDetailsNews } from '@/utils/getDetailsNews';
-import { Box, Container, Grid } from '@mui/material';
+import { Avatar, Box, Container, Grid, Typography } from '@mui/material';
 import Image from 'next/image';
 import React from 'react';
 
 const NewsDetailsPage = async({params}) => {
     const {data} = await getDetailsNews(params.newsId)
-    console.log(data)
     return (
-        <Box>
+        <Box className="my-20">
             <Container>
             <Grid container spacing={2}>
                   <Grid item xs={6}>
-                    <Image src={data.thumbnail_url} alt='complex'/>
-                    <Grid container spacing={2}>
+                    <Image src={data.thumbnail_url} width={800} height={500} alt='complex'/>
+                    <Grid className='mt-3' container spacing={2}>
                          <Grid item xs={6}>
-                           <Image src={data.image_url} alt='complex'/>
+                           <Image src={data.image_url} width={600} height={300} alt='complex'/>
                          </Grid>
                          <Grid item xs={6}>
-                           <Image src={data.image_url} alt='complex'/>
+                           <Image src={data.image_url} width={600} height={300} alt='complex'/>
                          </Grid>
                     </Grid>
                   </Grid>
                   <Grid item xs={6}>
-                    <Box>details </Box>
+                    <Typography gutterBottom variant='h4'>
+                        {data.title}
+                    </Typography>
+                    <Box sx={{
+                        display:"flex",
+                        alignItems:"center",
+                        gap:1,
+                    }}>
+                    <Avatar alt="Remy Sharp" src={data.author.img} />
+                    <Typography gutterBottom>
+                      By {data.author.name} 
+                    </Typography>
+                    <Typography gutterBottom>
+                        - {data.author.published_date}
+                    </Typography>
+                    </Box>
+                    <Typography style={{
+                        textAlign:"justify",
+                        whiteSpace:"pre-line",
+                        margin: "10px 5px"
+                    }}>
+                        {data.details}
+                    </Typography>
                   </Grid>
             </Grid>
             </Container>
