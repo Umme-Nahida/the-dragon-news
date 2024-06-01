cb59df7baed6c0a92a28c7a99b258b358ba5c589
Umme-Nahida <mysuccessmission@gmail.com>
2024-06-01 18:16:42 +0600

    added details content and author name and image

--- src/app/[news]/[newsId]/page.js
+++ src/app/[news]/[newsId]/page.js
@@ -6,7 +6,7 @@ import React from 'react';
 const NewsDetailsPage = async({params}) => {
     const {data} = await getDetailsNews(params.newsId)
     return (
-        <Box className="">
+        <Box className="20">
             <Container>
             <Grid container spacing={2}>
                   <Grid item xs={6}>
