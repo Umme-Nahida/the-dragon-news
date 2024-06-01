bb3b02d7bf8b38e3424bca2d3778f8da020fac2e
Umme-Nahida <mysuccessmission@gmail.com>
2024-06-01 17:52:11 +0600

    added news details dynamic route

--- src/app/[news]/[newsId]/page.js
+++ src/app/[news]/[newsId]/page.js
@@ -1,13 +1,32 @@
 import { getDetailsNews } from '@/utils/getDetailsNews';
+import { Box, Container, Grid } from '@mui/material';
+import Image from 'next/image';
 import React from 'react';
 
 const NewsDetailsPage = async({params}) => {
     const {data} = await getDetailsNews(params.newsId)
     console.log(data)
     return (
-        <div>
-           {params.newsId}
-        </div>
+        <Box>
+            <Container>
+            <Grid container spacing={2}>
+                  <Grid item xs={6}>
+                    <Image src={data.thumbnail_url} alt='complex'/>
+                    <Grid container spacing={2}>
+                         <Grid item xs={6}>
+                           <Image src={data.image_url} alt='complex'/>
+                         </Grid>
+                         <Grid item xs={6}>
+                           <Image src={data.image_url} alt='complex'/>
+                         </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={6}>
+                    details
+                  </Grid>
+            </Grid>
+            </Container>
+        </Box>
     );
 };
 
